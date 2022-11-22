d00824e4e4b3dfa9b1803fbdf29a8b2114b112fa
Sangwan Lee <saohwan@codemind.co.kr>
2022-11-22 21:50:48 +0900

    [2022-11-22(화)] hash 암호화 추가
    - Werkzeug를 사용하여 비밀번호 해싱하기

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from http.client import TEMPORARY_REDIRECT
+
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -30,16 +32,29 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-
+    hash_and_salted_password = generate_password_hash(
+        request.form.get('password'),
+        method="pbkdf2:sha256",
+        salt_length=8
+    )
     user = User()
     user.name = request.form.get('name')
     user.email = request.form.get('email')
-    user.password = request.form.get('password')
+    user.password = hash_and_salted_password
+    # user.password = request.form.get('password')
 
     if request.form:
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for("secrets"), code=307)
+
+        '''
+        51 Line Info
+        code=302 Default , code=307 값을 줘야 name 값을 가져 오는 이유는?
+        302는 기존 Method 와 Body 를 버리고 GET 방식으로 요청하는 반면,
+        307 은 받은 요청 Method 와 Body 를 유지한채로 재 요청 하기 때문에,
+        담겨 있던 name 이 정상적으로 secrets 페이지에 정상 적으로 출력.
+        '''
+        return redirect(url_for("secrets"), TEMPORARY_REDIRECT)
     return render_template("register.html")
 
 
